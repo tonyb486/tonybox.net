6757b97d8fb4a21fc48783cfa16da1bc581adeb1
Anthony Biondo <tonyb@tonybox.net>
2020-03-20 19:08:52 -0400

    Update covid19 post.

---
+++ content/posts/covid19/covid19.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import time, datetime
+import numpy as np
+
+country = "US"
+
+covidUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+covidData=pd.read_csv(covidUrl)
+
+usData = covidData.where(covidData["Country/Region"] == country).dropna(how="all")
+usDaily = pd.DataFrame(usData.sum(axis=0).iloc[4:])
+usDaily.index = pd.to_datetime(usDaily.index)
+
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(111)
+
+ax.plot(usDaily)
+ax.plot(usDaily, ".")
+
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=500, freq='W'), minor=False)
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=100, freq='D'), minor=True)
+ax.grid(which='major', alpha=0.9)
+ax.grid(which='minor', alpha=0.2)
+
+ax.set_xlabel("Date")
+ax.set_ylabel("Cases in %s"%(country))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+
+start = datetime.date.today() - datetime.timedelta(days=45)
+end = datetime.date.today() + datetime.timedelta(days=2)
+plt.xlim(start, end)
+
+plt.title("Cases of COVID-19 in %s\nGenerated on %s\nData From: https://github.com/CSSEGISandData/COVID-19/"%(country,time.strftime("%Y-%m-%d @ %H:%M %Z")))
+
+fig.patch.set_alpha(0.)
+
+fig.savefig("covid19.png", dpi=300)
+fig.savefig("covid19.svg", dpi=300)
+
+#### CURVE FIT
+
+import datetime
+import scipy.optimize
+x0,y0 = zip(*enumerate(list(usDaily[0])))
+
+f = lambda t, a, b: a**(t-b)
+(a,b), _ = scipy.optimize.curve_fit(f, x0, y0)
+
+startdate = datetime.date(2020, 1, 22) + datetime.timedelta(days=b)
+
+x = pd.date_range('2020-01-22', datetime.date.today()+ datetime.timedelta(days=4), freq='D')
+ax.plot(x, [f(i,a,b) for i,_ in enumerate(x)], ":", label="Exponential Curve Fit (Factor = %.2f, Start = %s)"%(a, startdate.strftime("%Y-%m-%d")))
+
+fig.patch.set_alpha(0.)
+
+start = datetime.date.today() - datetime.timedelta(days=25)
+end = datetime.date.today() + datetime.timedelta(days=6)
+plt.xlim(start, end)
+plt.title("Exponential Curve Fit for COVID-19 Cases in %s\nGenerated on %s\nData From: https://github.com/CSSEGISandData/COVID-19/"%(country, time.strftime("%Y-%m-%d @ %H:%M %Z")))
+plt.legend()
+
+fig.savefig("covid19-fit.png", dpi=300)
+fig.savefig("covid19-fit.svg", dpi=300)
+
+## Logistic Fits!
+
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(111)
+
+ax.plot(usDaily, label="Actual Data", linewidth=1)
+ax.plot(usDaily, ".")
+
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=100, freq='W'), minor=False)
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=100, freq='D'), minor=True)
+ax.grid(which='major', alpha=0.9)
+ax.grid(which='minor', alpha=0.2)
+
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=500, freq='W'), minor=False)
+ax.set_xticks(pd.date_range(usDaily.index[0], periods=100, freq='D'), minor=True)
+ax.grid(which='major', alpha=0.9)
+ax.grid(which='minor', alpha=0.2)
+
+ax.set_xlabel("Date")
+ax.set_ylabel("Cases in %s"%(country))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+
+start = datetime.date.today() - datetime.timedelta(days=30)
+end = datetime.date.today() + datetime.timedelta(days=15)
+plt.xlim(start, end)
+
+x = pd.date_range('2020-01-22', datetime.date.today()+ datetime.timedelta(days=15), freq='D')
+
+## First Logistic Fit
+
+fig.patch.set_alpha(0.)
+b = len(x0)+3
+fL = lambda t, L: L/(1 + np.exp((1-a)*(t-b)))
+L, _ = scipy.optimize.curve_fit(fL, x0, y0)
+
+startdate = datetime.date(2020, 1, 22) + datetime.timedelta(days=b)
+ax.plot(x, [fL(i,L) for i,_ in enumerate(x)], ":", label="Logistic Fit (%.2f, L=%d, Assuming Inflection ~%s)"%(a, L, startdate.strftime("%Y-%m-%d")))
+
+plt.title("Hypothetical Logistic Fit for COVID-19 Cases in %s\nGenerated on %s\nData From: https://github.com/CSSEGISandData/COVID-19/"%(country,time.strftime("%Y-%m-%d @ %H:%M %Z")))
+plt.legend()
+
+fig.savefig("covid19-logistic-fit.png", dpi=300)
+fig.savefig("covid19-logistic-fit.svg", dpi=300)
+
+## Second Logistic Fit
+
+b = len(x0)+10
+L, _ = scipy.optimize.curve_fit(fL, x0, y0)
+
+startdate = datetime.date(2020, 1, 22) + datetime.timedelta(days=b)
+ax.plot(x, [fL(i,L) for i,_ in enumerate(x)], ":", label="Logistic Fit (%.2f, L=%d, Assuming Inflection ~%s)"%(a, L, startdate.strftime("%Y-%m-%d")))
+plt.title("Hypothetical Logistic Fit for COVID-19 Cases in %s\nGenerated on %s\nData From: https://github.com/CSSEGISandData/COVID-19/"%(country,time.strftime("%Y-%m-%d @ %H:%M %Z")))
+plt.legend()
+
+fig.savefig("covid19-logistic-fit2.png", dpi=300)
+fig.savefig("covid19-logistic-fit2.svg", dpi=300)
---
+++ content/posts/covid19/index.md
@@ -0,0 +1,49 @@
+---
+title: "Some Charts of COVID-19 Cases in the US"
+date: 2020-03-19
+tags: ["Math", "Charts", "Programming"]
+description: "Some US COVID-19 Charts"
+summary: "Some charts showing COVID-19 cases in the US, including a general growth chart, an exponential curve fitting, and a couple of hypothetical logistic curve fittings."
+
+---
+
+Here's a simple chart, showing the growth of COVID-19, updated regularly from data published on <a href="https://github.com/CSSEGISandData/COVID-19/">github</a> by CSSE at Johns Hopkins University.  If you'd like more detailed information, they have an excellent [information center](https://coronavirus.jhu.edu/).
+
+<img alt="A chart based on the data from https://github.com/CSSEGISandData/COVID-19/" src="https://tmp.tonybox.net/covid19.svg" style="width: 100%">
+
+## Exponential Fit
+
+With scipy, we can fit an exponential curve to this data.  In this case, I'm only fitting two variables: the day the exponential growth "began," (*i.e.*, the date when there was at least 1 case expected by the curve, from which growth continued exponentially by the shown factor) shown as the start date in the legend below, and the factor by  which it has been growing since then, also shown in the legend:
+
+<img alt="A chart with an exponential curve fit, based on the data from https://github.com/CSSEGISandData/COVID-19/" src="https://tmp.tonybox.net/covid19-fit.svg" style="width: 100%">
+
+<center><small>
+
+(Fitting the function `y = r**(x - timedelta)`)
+
+</small></center>
+
+If it looks messed up, then maybe the exponential growth is over, or its possible that I've made a mistake. I'm no mathematician, so please don't rely on this data to make any decisions - its just for your information.
+
+## Hypothetical Logistic Fits
+
+Now, let's consider fitting a logistic function instead, because this will end. A logistic function starts out exponential, eventually giving way to a plateau.  We don't know when the plateau will happen, and we don't know when the curve will stop being exponential. I suspect that these are largely dependant on the action we take, and it is unlikely that it will only plateau when it infects the entire population.  
+
+These are not predictions, though, because we have to make an assumption about the inflection point, and I don't have enough data or the ability to make a real assumption about that. Instead, I'll propose a couple of hypothetical inflection points, which illustrate the effect that even a small change in this curve can have.
+
+It's also an issue that the current rate is probably inflated due to the growth of testing, and that makes hypotheticals even more unreliable as predictions.  
+
+With those caveats, we can see below the effect of assuming a hypothetical inflection point about ~3 days away, and using the other variables from our previous exponential fit:
+
+<img alt="A chart with an logistic curve fit, based on the data from https://github.com/CSSEGISandData/COVID-19/" src="https://tmp.tonybox.net/covid19-logistic-fit.svg" style="width: 100%">
+
+And here, we can see the effect of assuming a hypothetical inflection point about ~10 days away, which dramatically increases the expected number of total cases, L:
+
+<img alt="A chart with an logistic curve fit, based on the data from https://github.com/CSSEGISandData/COVID-19/" src="https://tmp.tonybox.net/covid19-logistic-fit2.svg" style="width: 100%">
+
+But I'm just a guy who saw [3Blue1Brown's excellent video](https://www.youtube.com/watch?v=Kas0tIxDvrg) on this topic and thought I could try this at home, so take these charts with a healthy heaping of salt.
+
+These graphs update automatically with the data published on github.  
+
+You can see the code that generates this [here](https://gist.github.com/tonyb486/c1eab7e683cea973b6242661fd68e9a9).
+
